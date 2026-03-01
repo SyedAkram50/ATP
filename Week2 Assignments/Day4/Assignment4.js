0e7c876b7c14260c066b30e6bca4f1ee68a9ec79
SyedAkram50 <24eg110a50@anurag.edu.in>
2026-03-01 18:56:47 +0530

    Create Assignment4.js

---
+++ Week2 Assignments/Day4/Assignment4.js
@@ -0,0 +1,21 @@
+const user = {
+    id: 101,
+    name: "Ravi",
+    preferences: {
+        theme: "dark",
+        language: "en"
+    }
+};
+
+//Shallow copying
+let copycuser={...user}
+
+//Changing name in the copied
+copycuser.name='Akram'
+
+//Changing preferences.theme in the copied
+copycuser.preferences.theme='bright'
+
+//printing
+console.log(user)
+console.log(copycuser)
