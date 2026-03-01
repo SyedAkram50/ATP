28061148528e69f8b231a26fc21b4294fea6641d
SyedAkram50 <24eg110a50@anurag.edu.in>
2026-03-01 18:57:34 +0530

    Update Assignment2.js

--- Week2 Assignments/Day4/Assignment2.js
+++ Week2 Assignments/Day4/Assignment2.js
@@ -1,7 +1,9 @@
 //Creating an array fruits
 let fruits = ["apple", "banana"]
+
 //shallow copying the friuts and adding orange
 let moreFruits=[...fruits,'orange']
+
 //printing
 console.log(fruits)
 console.log(moreFruits)
