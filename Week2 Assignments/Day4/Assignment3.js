158af56a2630b3eb20c966991a49603ded26e77d
SyedAkram50 <24eg110a50@anurag.edu.in>
2026-03-01 18:57:19 +0530

    Update Assignment3.js

--- Week2 Assignments/Day4/Assignment3.js
+++ Week2 Assignments/Day4/Assignment3.js
@@ -3,8 +3,10 @@ let user = {
   name: "Ravi",
   city: "Hyderabad"
 };
+
 //shallow copying and adding a new property
 let updatedUser={...user,age: 25}
+
 //printing
 console.log(user)
 console.log(updatedUser)
