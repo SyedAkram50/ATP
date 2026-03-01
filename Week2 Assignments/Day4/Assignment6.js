8bde4a7d86fec892c0e14ad361bad4cdff671bde
SyedAkram50 <24eg110a50@anurag.edu.in>
2026-03-01 19:05:10 +0530

    Create Assignment6.js

---
+++ Week2 Assignments/Day4/Assignment6.js
@@ -0,0 +1,8 @@
+//Exam portal simulator:
+console.log("Exam submitted successfully")
+
+//After 2 seconds → show: “Evaluating answers…”
+setTimeout(()=>{console.log("Evaluating answers...")},2000)
+
+//After 4 seconds → show: “Result: Pass”
+setTimeout(()=>{console.log("Result: Pass")},4000)
